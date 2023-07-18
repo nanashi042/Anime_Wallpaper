05b7765d80504087b5b3732d3b5f0438f223b8df
nanashi <ny1113548@gmail.com>
2023-07-19 00:40:28 +0530

    none

--- static/js/main.js
+++ static/js/main.js
@@ -1,6 +1,7 @@
 var gallery = document.querySelector('#gallery');
 var getVal = function (elem, style) { return parseInt(window.getComputedStyle(elem).getPropertyValue(style)); };
 var getHeight = function (item) { return item.querySelector('.content').getBoundingClientRect().height; };
+
 var resizeAll = function () {
     var altura = getVal(gallery, 'grid-auto-rows');
     var gap = getVal(gallery, 'grid-row-gap');
@@ -12,16 +13,19 @@ var resizeAll = function () {
 gallery.querySelectorAll('img').forEach(function (item) {
     item.classList.add('byebye');
     if (item.complete) {
+
         console.log(item.src);
         item.addEventListener('load', function () {
+            // javascript:location.reload(true)
             var altura = getVal(gallery, 'grid-auto-rows');
             var gap = getVal(gallery, 'grid-row-gap');
             var gitem = item.parentElement.parentElement;
             gitem.style.gridRowEnd = "span " + Math.ceil((getHeight(gitem) + gap) / (altura + gap));
-            item.classList.remove('byebye');
-        });
+            item.classList.remove('byebye')        });
     }
     else {
+        // javascript:location.reload(true)
+   
         item.addEventListener('load', function () {
             var altura = getVal(gallery, 'grid-auto-rows');
             var gap = getVal(gallery, 'grid-row-gap');
